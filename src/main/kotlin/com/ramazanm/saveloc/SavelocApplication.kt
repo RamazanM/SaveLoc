666b9ed1c45a5b482121324bf6575b13d44481df
RamazanM <r.ramazanmutlu@gmail.com>
2025-10-07 13:02:55 +0300

    Initial commit. Spring boot initialized.

---
+++ src/main/kotlin/com/ramazanm/saveloc/SavelocApplication.kt
@@ -0,0 +1,11 @@
+package com.ramazanm.saveloc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SavelocApplication
+
+fun main(args: Array<String>) {
+	runApplication<SavelocApplication>(*args)
+}
---
+++ src/test/kotlin/com/ramazanm/saveloc/SavelocApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ramazanm.saveloc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SavelocApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
