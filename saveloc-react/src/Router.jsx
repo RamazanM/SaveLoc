fe60a67b22f9b46423df0a42836e52fe12627058
RamazanM <r.ramazanmutlu@gmail.com>
2025-10-12 12:18:54 +0300

    feature(FE): React router has been added and test routes created.

---
+++ saveloc-react/src/Router.jsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter, RouterProvider, Outlet, Link } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <>
+        <div>
+          Header
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Outlet />
+      </>
+    ),
+    children: [
+      { index: true, element: <>Home</> },
+      { path: 'login', element: <>Login</> },
+      { path: 'register', element: <>Register</> },
+    ],
+  },
+])
+
+export default function AppRouter() {
+  return <RouterProvider router={router} />
+}
